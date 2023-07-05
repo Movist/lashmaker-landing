95885765d7b6027a0740e3c61c8aa64ac967858c
movist <shtigun99@gmail.com>
2023-07-05 12:42:28 +0300

    version 0.1

---
+++ src/main/java/com/example/lashmakerlanding/LashmakerLandingApplication.java
@@ -0,0 +1,13 @@
+package com.example.lashmakerlanding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LashmakerLandingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LashmakerLandingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/lashmakerlanding/LashmakerLandingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lashmakerlanding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LashmakerLandingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
